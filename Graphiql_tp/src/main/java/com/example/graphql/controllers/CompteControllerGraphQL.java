5cd81d144792c27f3a39c0e2156e3df047af0508
@Neha-ETTALEBY <ettaleby.neha@gmail.com>
2024-11-22 15:31:54 +0100

    first

---
+++ Graphiql_tp/src/main/java/com/example/graphql/controllers/CompteControllerGraphQL.java
@@ -0,0 +1,64 @@
+package com.example.graphql.controllers;
+
+import com.example.graphql.entities.Compte;
+import com.example.graphql.entities.TypeCompte;
+import com.example.graphql.repositories.CompteRepository;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
+import org.springframework.stereotype.Controller;
+import lombok.AllArgsConstructor;
+
+import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
+
+@Controller
+@AllArgsConstructor
+public class CompteControllerGraphQL {
+
+    private CompteRepository compteRepository;
+
+    @QueryMapping
+    public List<Compte> allComptes() {
+        return compteRepository.findAll();
+    }
+
+    @QueryMapping
+    public Compte compteById(@Argument Long id) {
+        return compteRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException(String.format("Compte %s not found", id)));
+    }
+
+    @MutationMapping
+    public Compte saveCompte(@Argument Compte compte) {
+        return compteRepository.save(compte);
+    }
+
+    @QueryMapping
+    public Map<String, Object> totalSolde() {
+        long count = compteRepository.count();
+        double sum = compteRepository.sumSoldes();
+        double average = count > 0 ? sum / count : 0;
+
+        Map<String, Object> result = new HashMap<>();
+        result.put("count", count);
+        result.put("sum", sum);
+        result.put("average", average);
+
+        return result;
+    }
+    @MutationMapping
+    public boolean deleteByType(@Argument TypeCompte type) {
+        List<Compte> comptesToDelete = compteRepository.findByType(type);
+        if (comptesToDelete.isEmpty()) {
+            throw new RuntimeException(String.format("No comptes found with type %s", type));
+        }
+        compteRepository.deleteAll(comptesToDelete);
+        return true;
+    }
+    @QueryMapping
+    public List<Compte> searchByType(@Argument TypeCompte type) {
+        return compteRepository.findByType(type);
+    }
+}
